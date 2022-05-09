607e80b6a1d2131e9b80b02a625e9da7ffe5050b
AndreiArsiomenko <andartsiomenko@gmail.com>
2022-05-10 02:19:32 +0300

    feat: add basic configurations webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: {
+      keep: /\.git$/i,
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Virtual keyboard',
+      filename: 'index.[contenthash].html',
+      template: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    new ESLintPlugin()
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: [MiniCssExtractPlugin.loader, 'css-loader'],
+    }, ],
+  },
+  devServer: {
+    static: './dist',
+  },
+};
